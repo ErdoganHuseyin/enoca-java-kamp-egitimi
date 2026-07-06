e462ad3ae2aa3afe1f936cd14f6d0002b601739b
Hüseyin Erdoğan <huseyinerdogan426@gmail.com>
2026-07-06 15:58:19 +0300

    feat: OOP temelleri, encapsulation ve inheritance yapısı kuruldu

---
+++ src/CorporateCustomer.java
@@ -0,0 +1,11 @@
+public class CorporateCustomer extends Customer{
+private String companyName;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,45 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+Product product;
+product = new Product();
+   product.setId(1);
+    product.setName("Delonghi");
+    product.setPrice(7500);
+    product.setStock(200);
+Product product1 = new Product();
+product1.setId(2);
+product1.setName("Smeg");
+product1.setPrice(6000);
+product1.setStock(100);
+
+Product product2 = new Product();
+product2.setName("Kitchen Aid");
+product2.setId(3);
+product2.setPrice(5000);
+product2.setStock(150);
+
+    Product[] products = {product,product1,product2};
+
+    for(Product product3 : products){
+        System.out.println(product3.getName());
+    }
+
+
+    IndividualCustomer individualCustomer = new IndividualCustomer();
+    individualCustomer.setCustomerNumber("12345");
+    individualCustomer.setId(1);
+    individualCustomer.setPhone("05343961024");
+    individualCustomer.setFirstName("Hüseyin");
+    individualCustomer.setLastName("Erdoğan");
+
+
+    CorporateCustomer corporateCustomer = new CorporateCustomer();
+    corporateCustomer.setId(2);
+    corporateCustomer.setCustomerNumber("12378");
+    corporateCustomer.setCompanyName("Enoca");
+    corporateCustomer.setPhone("055555555555");
+
+    Customer[] customers ={individualCustomer,corporateCustomer};
+
+}
